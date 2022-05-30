15cf90a81b8252d360a59615d417814b17ac21e7
vlad.smelianets <vlad.smelianets@yandex.ru>
2022-05-30 18:24:44 +0300

    feat: add params and option to differ

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,21 @@
 package hexlet.code;
 
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public final class Differ implements Runnable {
+
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private String format;
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String firstFilePath;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String secondFilePath;
 
     public static void generate() {
         System.out.println("Placeholder for Differ.generate");
