df351065ce973e29905a91408221220233dce829
vlad.smelianets <vlad.smelianets@yandex.ru>
2022-06-14 14:12:36 +0300

    test: rework DifferTest to parametrized, rename fixtures

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -3,78 +3,69 @@ package hexlet.code;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.util.Files;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URISyntaxException;
 
 class DifferTest {
 
-    private String jsonPath1 = new File(getClass().getClassLoader().getResource("nested-json-fixture-1.json")
-            .toURI()).toString();
-    private String jsonPath2 = new File(getClass().getClassLoader().getResource("nested-json-fixture-2.json")
-            .toURI()).toString();
-    private String yamlPath1 = new File(getClass().getClassLoader().getResource("nested-yaml-fixture-1.yml")
-            .toURI()).toString();
-    private String yamlPath2 = new File(getClass().getClassLoader().getResource("nested-yaml-fixture-2.yml")
-            .toURI()).toString();
-
-    private String expectedStylish = Files.contentOf(
+    private final String expectedStylish = Files.contentOf(
             new File(getClass().getClassLoader().getResource("expected-stylish.txt").toURI()), "UTF-8");
-    private String expectedPlain = Files.contentOf(
+    private final String expectedPlain = Files.contentOf(
             new File(getClass().getClassLoader().getResource("expected-plain.txt").toURI()), "UTF-8");
-    private String expectedJson = Files.contentOf(
+    private final String expectedJson = Files.contentOf(
             new File(getClass().getClassLoader().getResource("expected-json.txt").toURI()), "UTF-8");
 
     DifferTest() throws URISyntaxException {
     }
 
-    @Test
-    @DisplayName("Should return stylish diff when given jsons and stylish")
-    void shouldReturnStylishDiffWhenJsonsAndStylish() throws IOException {
-        Assertions.assertThat(Differ.generate(jsonPath1, jsonPath2, "stylish")).isEqualTo(expectedStylish);
+    @ParameterizedTest
+    @DisplayName("Should return stylish diff when given stylish format param")
+    @ValueSource(strings = {"json", "yml"})
+    void shouldReturnStylishDiffWhenGivenStylishFormatParam(String format) throws Exception {
+        String filePath1 = new File(getClass().getClassLoader().getResource("nested-fixture-1." + format)
+                .toURI()).toString();
+        String filePath2 = new File(getClass().getClassLoader().getResource("nested-fixture-2." + format)
+                .toURI()).toString();
+
+        Assertions.assertThat(Differ.generate(filePath1, filePath2, "stylish")).isEqualTo(expectedStylish);
     }
 
-    @Test
-    @DisplayName("Should return plain diff when given jsons and plain")
-    void shouldReturnPlainDiffWhenJsonsAndPlain() throws IOException {
-        Assertions.assertThat(Differ.generate(jsonPath1, jsonPath2, "plain")).isEqualTo(expectedPlain);
+    @ParameterizedTest
+    @DisplayName("Should return plain diff when given plain format param")
+    @ValueSource(strings = {"json", "yml"})
+    void shouldReturnPlainDiffWhenGivenPlainFormatParam(String format) throws Exception {
+        String filePath1 = new File(getClass().getClassLoader().getResource("nested-fixture-1." + format)
+                .toURI()).toString();
+        String filePath2 = new File(getClass().getClassLoader().getResource("nested-fixture-2." + format)
+                .toURI()).toString();
+
+        Assertions.assertThat(Differ.generate(filePath1, filePath2, "plain")).isEqualTo(expectedPlain);
     }
 
-    @Test
-    @DisplayName("Should return json diff when given jsons and json")
-    void shouldReturnJsonDiffWhenJsonsAndJson() throws IOException {
-        Assertions.assertThat(Differ.generate(jsonPath1, jsonPath2, "json")).isEqualTo(expectedJson);
+    @ParameterizedTest
+    @DisplayName("Should return json diff when given json format param")
+    @ValueSource(strings = {"json", "yml"})
+    void shouldReturnJsonDiffWhenGivenJsonFormatParam(String format) throws Exception {
+        String filePath1 = new File(getClass().getClassLoader().getResource("nested-fixture-1." + format)
+                .toURI()).toString();
+        String filePath2 = new File(getClass().getClassLoader().getResource("nested-fixture-2." + format)
+                .toURI()).toString();
+
+        Assertions.assertThat(Differ.generate(filePath1, filePath2, "json")).isEqualTo(expectedJson);
     }
 
-    @Test
-    @DisplayName("Should return stylish diff when given yamls and stylish")
-    void shouldReturnStylishDiffWhenYamlsAndStylish() throws IOException {
-        Assertions.assertThat(Differ.generate(yamlPath1, yamlPath2, "stylish")).isEqualTo(expectedStylish);
-    }
+    @ParameterizedTest
+    @DisplayName("Should return stylish diff by default when without format param")
+    @ValueSource(strings = {"json", "yml"})
+    void shouldReturnStylishDiffByDefault(String format) throws Exception {
+        String filePath1 = new File(getClass().getClassLoader().getResource("nested-fixture-1." + format)
+                .toURI()).toString();
+        String filePath2 = new File(getClass().getClassLoader().getResource("nested-fixture-2." + format)
+                .toURI()).toString();
 
-    @Test
-    @DisplayName("Should return plain diff when given yamls and plain")
-    void shouldReturnPlainDiffWhenYamlsAndPlain() throws IOException {
-        Assertions.assertThat(Differ.generate(yamlPath1, yamlPath2, "plain")).isEqualTo(expectedPlain);
-    }
-
-    @Test
-    @DisplayName("Should return json diff when given yamls and json")
-    void shouldReturnJsonDiffWhenYamlsAndJson() throws IOException {
-        Assertions.assertThat(Differ.generate(yamlPath1, yamlPath2, "json")).isEqualTo(expectedJson);
-    }
-
-    @Test
-    @DisplayName("Should return stylish diff by default when given yamls without format param")
-    void shouldReturnStylishDiffByDefaultWhenYamls() throws IOException {
-        Assertions.assertThat(Differ.generate(yamlPath1, yamlPath2)).isEqualTo(expectedStylish);
-    }
-
-    @Test
-    @DisplayName("Should return stylish diff by default when given yamls without format param")
-    void shouldReturnStylishDiffByDefaultWhenJsons() throws IOException {
-        Assertions.assertThat(Differ.generate(jsonPath1, jsonPath2)).isEqualTo(expectedStylish);
+        Assertions.assertThat(Differ.generate(filePath1, filePath2)).isEqualTo(expectedStylish);
     }
 }
